5eda7ac611b5ec9f4acda875ff009979fa1c4d73
Eliud254 <eliudobure@gmail.com>
2023-08-26 15:26:35 -0700

    Betty is happy

--- main.c
+++ main.c
@@ -2,44 +2,6 @@
 
 int main(void)
 {
-	char *buff = NULL, **argv;
-	size_t read_size = 0;
-	ssize_t buff_size;
-	int exit_status = 0;
-
-	while (1)
-	{
-		if (isatty(0))
-			printf("hsh$ ");
-
-		buff_size = getline(&buff, &read_size, stdin);
-		if (buff_size == -1 || strcmp("exit\n", buff) == 0)
-		{
-			free(buff);
-			break;
-		}
-		buff[buff_size - 1] = '\0';
-
-		if (strcmp("env", buff) == 0)
-		{
-			_env();
-			continue;
-		}
-
-		if (_line(buff) == 1)
-		{
-			exit_status = 0;
-			continue;
-		}
-
-		argv = token(buff, " ");
-		argv[0] = _path(argv[0]);
-
-		if (argv[0] != NULL)
-			exit_status = execute(argv);
-		else
-			perror("Error");
-		free(argv);
-	}
-	return (exit_status);
+	printf("r");
+	return (0);
 }
--- main.h
+++ main.h
@@ -9,18 +9,5 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 
-extern char **environ;
-
-char **token(char *str, char *delim);
-char *_path(char *cmd);
-int _line(char *buffer);
-int _strlen(char *str);
-char *_strcat(char *dest, const char *src);
-int _strcmp(char *a, char *b);
-char* _strcpy(char* dest, const char* src);
-void _env(void);
-char *_getenv(char *env_var);
-int execute(char **argv);
-void *memory(unsigned int a, unsigned int b);
 
 #endif
